f4f8e74fdd05eee166b3a8a2c2e810ce47cd54a9
caprice-j <skyfly0008@gmail.com>
2017-01-01 02:06:10 -0500

    add tests

--- tests/testthat/test-builtin.R
+++ tests/testthat/test-builtin.R
@@ -5,9 +5,24 @@ test_that('builtins', {
     e <- execute_ggbash_builtins
     const <- define_constant_list()
     oldwd <- getwd()
+    rmdir <- function(dir) unlink(dir, recursive = TRUE)
     # Can I assume some particular directory here?
     expect_message(e(  'pwd',   'pwd', const, iris))
     expect_message(e('getwd', 'getwd', const, iris))
+
+    gibberish <- 'badcfeghzfumfduqkm'
+    expect_error(e(paste0('mkdir ', gibberish),
+                   c('mkdir', gibberish), const, iris), regexp=NA)
+    rmdir(gibberish)
+    expect_error(e(paste0('dir.create ', gibberish),
+                   c('dir.create', gibberish), const, iris), regexp=NA)
+    expect_error(e(paste0('rm ', gibberish),
+                 c('rm', gibberish), const, iris), 'this is a directory')
+    rmdir(gibberish)
+
+    file.create(gibberish)
+    expect_error(e(paste0('rmdir ',gibberish),
+                   c('rmdir', gibberish), const, iris), 'this is not a directory')
 
     expect_message(e('list', 'list', const, iris))
     expect_message(e( 'str',  'str', const, iris))
--- tests/testthat/test-save_ggplot.R
+++ tests/testthat/test-save_ggplot.R
@@ -14,5 +14,8 @@ test_that('save png', {
     e(save_ggplot(datadirv[1], statl, argv=c('png', 'small')))
     e(save_ggplot(datadirv[1], statl, argv=c('png', '500x250')))
     e(save_ggplot(datadirv[1], statl, argv=c('png', '500x250', '"my-filename"')))
+    argv <- c('500x250', '"my-filename"', '50')
+    for (indices in list(c(1,2,3), c(1,3,2), c(2,1,3), c(2,3,1), c(3,1,2), c(3,2,1)))
+        e(save_ggplot(datadirv[1], statl, argv=c('png', argv[indices])))
     unlink(x = paste0(tmpdir,'/',datadirv[1]), recursive = TRUE)
 })
