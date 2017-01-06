beb16a309f0155d3e313d3e02f44640365ebe1a0
caprice-j <skyfly0008@gmail.com>
2017-01-05 19:31:29 -0500

    refactor lexer.R and parser.R

--- R/lexer.R
+++ R/lexer.R
@@ -13,50 +13,51 @@ GGPLOT2_LITERALS = c() # needed?
 # MAYBE-LATER don't know how to pass variables between yacc's production rules
 ggbashenv <- new.env() # Note: This is a global variable.
 
-Ggplot2Lexer <- R6::R6Class("Lexer",
-                 public = list(
-                     tokens = GGPLOT2_TOKENS,
-                     literals = GGPLOT2_LITERALS,
-                     #states = list(c('ggplot')),
-                     # Note: t_(function) defines precedences implicitly
-                     t_GGPLOT = function(re='^(g|gg|ggp|ggpl|ggplo|ggplot)\\s+[a-zA-Z_][a-zA-Z_0-9\\.]*', t) {
-                         t$value <- gsub('^g(g|gp|gpl|gplo|gplot)?\\s*', 'ggplot2::ggplot(', t$value)
-                         return(t)
-                     },
-                     t_CONSTAES = paste0('[a-z]+=[a-zA-Z0-9]+'),
-                     # I believe CONSTAES cannot contain +-*/^,
-                     # because 'gg iris + point Sepal.W Sepal.L size=4 + smooth colour="blue"'
-                     # will be interpreted as LexToken(CHARAES,colour="blue" size=4 + smooth colour="blue",1,33)
-                     t_CHARAES = paste0('[a-z]+=("|', "'", ').*?("|', "'", ')'),
-                     t_NAME = '[a-zA-Z_][a-zA-Z_0-9\\.=]*',
-                     #t_LPAREN  = '\\(',
-                     #t_RPAREN  = '\\)',
-                     #t_COMMA = ',',
-                     t_LAYER = function(re='(\\+|\\|)\\s*[a-z_]+', t) {
-                         partial <- gsub('\\s*(\\+|\\|)\\s*(geom_)?', '', t$value)
-                         ggbashenv$const <- define_ggbash_constant_list()
-                         # FIXME showWarn
-                         ggbashenv$showWarn <- TRUE
-                         geom_sth <- ggbashenv$const$geom_namev[find_first(partial,
-                                                                           ggbashenv$const$geom_namev,
-                                                                           ggbashenv$showWarn)]
+Ggplot2Lexer <-
+    R6::R6Class("Lexer",
+                public = list(
+                    tokens = GGPLOT2_TOKENS,
+                    literals = GGPLOT2_LITERALS,
+                    #states = list(c('ggplot')),
+                    # Note: t_(function) defines precedences implicitly
+                    t_GGPLOT = function(re='^(g|gg|ggp|ggpl|ggplo|ggplot)\\s+[a-zA-Z_][a-zA-Z_0-9\\.]*', t) {
+                        t$value <- gsub('^g(g|gp|gpl|gplo|gplot)?\\s*', 'ggplot2::ggplot(', t$value)
+                        return(t)
+                    },
+                    t_CONSTAES = paste0('[a-z]+=[a-zA-Z0-9]+'),
+                    # I believe CONSTAES cannot contain +-*/^,
+                    # because 'gg iris + point Sepal.W Sepal.L size=4 + smooth colour="blue"'
+                    # will be interpreted as LexToken(CHARAES,colour="blue" size=4 + smooth colour="blue",1,33)
+                    t_CHARAES = paste0('[a-z]+=("|', "'", ').*?("|', "'", ')'),
+                    t_NAME = '[a-zA-Z_][a-zA-Z_0-9\\.=]*',
+                    #t_LPAREN  = '\\(',
+                    #t_RPAREN  = '\\)',
+                    #t_COMMA = ',',
+                    t_LAYER = function(re='(\\+|\\|)\\s*[a-z_]+', t) {
+                        partial <- gsub('\\s*(\\+|\\|)\\s*(geom_)?', '', t$value)
+                        ggbashenv$const <- define_ggbash_constant_list()
+                        # FIXME showWarn
+                        ggbashenv$showWarn <- TRUE
+                        geom_sth <- ggbashenv$const$geom_namev[find_first(partial,
+                                                                          ggbashenv$const$geom_namev,
+                                                                          ggbashenv$showWarn)]
 
-                         t$value <- paste0(' + geom_', geom_sth)
-                         return(t)
-                     },
-                     t_NUMBER = function(re='\\d+', t) {
-                         t$value <- strtoi(t$value)
-                         return(t)
-                     },
-                     t_ignore = " \t",
-                     t_newline = function(re='\\n+', t) {
-                         t$lexer$lineno <- t$lexer$lineno + nchar(t$value)
-                         return(NULL)
-                     },
-                     t_error = function(t) {
-                         cat(sprintf("Illegal character '%s'", t$value[1]))
-                         t$lexer$skip(1)
-                         return(t)
-                     }
-                 )
-)
+                        t$value <- paste0(' + geom_', geom_sth)
+                        return(t)
+                    },
+                    t_NUMBER = function(re='\\d+', t) {
+                        t$value <- strtoi(t$value)
+                        return(t)
+                    },
+                    t_ignore = " \t",
+                    t_newline = function(re='\\n+', t) {
+                        t$lexer$lineno <- t$lexer$lineno + nchar(t$value)
+                        return(NULL)
+                    },
+                    t_error = function(t) {
+                        cat(sprintf("Illegal character '%s'", t$value[1]))
+                        t$lexer$skip(1)
+                        return(t)
+                    }
+                )
+    )
