1e5c02ae5caac8a6e0f7e50902b3181c1b27c62a
caprice-j <tnk.yasutaka@gmail.com>
2017-01-12 23:01:12 -0500

    considering NSE for ggbash interface

---
+++ lab/ggbash-operator.R
@@ -0,0 +1,19 @@
+`%>>%` <- function(e1, e2, ...) #ggbash(substitute(e2))
+
+#`%>>%` <- function(e1, ...) print(substitute(...))#ggbash(substitute(e2))
+
+
+`%>>%` <- function(e1, ...) {
+    print(e1)
+    #sapply(list(substitute(...)), function(x) { substitute(x)})
+    for (i in 1:2) {
+        print(substitute(...[[i]]))
+    }
+}
+
+gggbash <- function(x) {
+    substitute(x)
+}
+
+gggbash(gg(iris) + point(x=Sepal.W, y=Sepal.L) + theme(text(size=20), l.t.x(size=15)))
+
