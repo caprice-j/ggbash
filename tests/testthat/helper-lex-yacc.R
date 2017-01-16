f3ad48afe34544a7f5d4ef461df7b0774e1a8881
caprice-j <tnk.yasutaka@gmail.com>
2017-01-16 18:56:42 -0500

    make compiler codes modular

--- tests/testthat/helper-lex-yacc.R
+++ tests/testthat/helper-lex-yacc.R
@@ -1,39 +1,6 @@
 
 # This file will be executed before other test-* files.
 
-lex  <- rly::lex(Ggplot2Lexer)
-yacc <- rly::yacc(Ggplot2Parser)
-
-gbash <- function(ggbash_symbols) {
-    is_string <- tryCatch(class(ggbash_symbols) == "character",
-                          error = function(err) {FALSE})
-    if (is_string) {
-        cmd <- ggbash_symbols
-    } else {
-        raw_cmd <- deparse(substitute(ggbash_symbols),
-                           width.cutoff = 500) # arbitrary large
-        cmd <- raw_cmd
-    }
-    compile_ggbash(cmd)
-}
-
-# FIXME duplicate
-bash <- function(ggbash_symbols) {
-    is_string <- tryCatch(class(ggbash_symbols) == "character",
-                          error = function(err) {FALSE})
-    if (is_string) {
-        cmd <- ggbash_symbols
-    } else {
-        raw_cmd <- deparse(substitute(ggbash_symbols),
-                           width.cutoff = 500) # arbitrary large
-        cmd <- raw_cmd
-    }
-    gsub("ggplot2::", "", compile_ggbash(cmd))
-}
-
-
-
-
 `%+%` <- function(left, right) paste0(left, right)
 `%++%` <- function(left, right) paste0(left, right)
 
